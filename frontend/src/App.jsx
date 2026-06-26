ce425ef1bdc37f06e661ade71182b194248c0bd9
sanskar <sanskarvns19@gmail.com>
2026-06-26 18:42:36 +0530

    keycloak Enabaled

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
 // import './App.css'
 import { BrowserRouter as Router, Navigate, Routes, Route, useLocation } from 'react-router-dom'
 import { Button } from '@mui/material'
+import { AuthContext } from 'react-oauth2-code-pkce';
+import { useContext, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setCredentials } from './store/authSlice';
 function App() {
   
+  const { token , tokenData , login , logOut , isAuthenticated } = useContext(AuthContext);
+const dispatch = useDispatch();
+const [authReady , setAuthReady] = useState(false);
+
+useEffect(() => {
+if(token){
+  dispatch(setCredentialsals({token,user:tokenData}));
+  setAuthReady(true);
+}
+},[token,tokenData,dispatch]);
 
   return (
    <Router>
-    <Button variant="contained" color="#dc004e">
+    <Button variant="contained" onClick={()=> {login()}}>
   Login
 </Button>
     </Router>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,15 @@ import { Provider } from 'react-redux'
 import { store } from './store/store'
 
 import App from './App'
+import { AuthProvider } from 'react-oauth2-code-pkce'
+import { authConfig } from './store/authConfig'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
+  <AuthProvider authConfig={authConfig}>
   <Provider store={store}>
     <App />
-  </Provider>,
+  </Provider>
+  </AuthProvider>,
 )
